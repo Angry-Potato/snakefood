9ccdb8d912e0510fd8d85b2b75c494a3bf0d060a
blais <blais@localhost>
2008-06-01 17:10:41 -0400

    Create talk outline/slides.

---
+++ test/snakefood/checker/test_checker.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Functional test for Python checker.
+"""
+
+from snakefood.checker import *
+from snakefood.find import ImportVisitor
+
+
+def visit_source(source, cls):
+    mod = compiler.parse(source)
+    vis = cls()
+    compiler.walk(mod, vis)
+    return vis.finalize()
+
+_import_tests = (
+    ('import mymod', ['mymod']),
+    ('import mod1, mod2, mod3', ['mod1', 'mod2','mod3']),
+    ('import mod1 as mymod', ['mymod']),
+    ('import mod1 as mymod, mod2 as mymod2', ['mymod', 'mymod2']),
+    ('from mod1 import mymod', ['mymod']),
+    ('from mod1 import mymod, mymod2', ['mymod', 'mymod2']),
+    ('from mod1 import mymod as bli1', ['bli1']),
+    ('from mod1 import mymod as bli1, mymod2 as bli2', ['bli1', 'bli2']),
+    ('import os.path', ['os.path']),
+    )
+
+_names_tests = (
+    ('fn = os.path.join("a", "b")', ['os', 'os.path', 'os.path.join']),
+    ('a = 1; print b.c ; d.e = 3', ['b', 'b.c', 'd']),
+    )
+
+def test():
+    for source, expected in _import_tests:
+        found = visit_source(source, ImportVisitor)
+        actual = [x[2] for x in found if x[2] is not None]
+        assert actual == expected, (actual, expected)
+
+    for source, expected in _names_tests:
+        dotted, simple = visit_source(source, NamesVisitor)
+        actual = [x[0] for x in dotted]
+        assert actual == expected, (actual, expected)
+
+if __name__ == '__main__':
+    test()
+
+
---
+++ test/snakefood/simple/notfound.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Module that cannot be found.
+import alien_module_yeah
+
+# Package that cannot be found.
+import alien_package.alien_module
+
+# From variants.
+from alien_module_yeah import alien_symbol
+from alien_package import alien_module
