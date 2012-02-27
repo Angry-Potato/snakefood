026a73f6675ebe865e7621b9aee132186bae03ac
Martin Blais <mblais@marketfactory.com>
2012-02-27 18:49:42 -0500

    More general.

---
+++ test/test_various.py
@@ -0,0 +1,27 @@
+"""
+Various tests.
+"""
+
+from os.path import *
+from testsupport import *
+
+
+_files = [
+    'simple/stdlib.py',
+    'simple/invalid.py',
+    'simple/notfound.py',
+    'project/foo_import.py',
+    'project/foo_from.py',
+    'project/sub1/sub11/relative_from.py',
+    ]
+
+def test_various():
+    "Test ignoring unused imports."
+
+    for fn in _files:
+        fn = join(data, fn)
+        print 'Testing for: %s' % fn
+        compare_expect(fn.replace('.py', '.expect'), None,
+                       'sfood', fn, filterdir=(data, 'ROOT'))
+
+
